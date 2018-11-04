d19d20ae66623d676f4171ab062cdd96d9bff274
Vitor Takara <vtakaraa@gmail.com>
2018-11-04 21:14:50 -0200

    initial commit

---
+++ app/js/menu.js
@@ -0,0 +1,13 @@
+<script>
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
+</script>
