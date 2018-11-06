509b25f58520e2d83a58760042be3f4cae50c973
VitorTakara <vtakaraa@gmail.com>
2018-11-06 16:52:04 -0200

    Galeria com animate.css

--- app/js/masonry.js
+++ app/js/masonry.js
@@ -1,18 +1,31 @@
 // vanilla JS
 // init with element
 var grid = document.querySelector('.grid');
-var msnry = new Masonry( grid, {
-  // options...
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
+var msnry = new Masonry( grid, {});
 
-// init with selector
-var msnry = new Masonry( '.grid', {
-  // options...
-});
 
-function test(e) {
-  e.parentNode.removeChild(e);
-  $grid.masonry()
+function filtrarMasonry(portfolioType) {
+	let masonry = document.querySelector(".masonry").querySelectorAll(".grid-item");
+
+	if ( portfolioType == "todos" )
+		masonry.forEach(i => {
+			i.classList.add("fadeIn");
+			i.classList.remove("display-none");
+		})
+	else
+		masonry.forEach(i => {
+			if(!i.classList.contains(portfolioType)){
+				if(i.classList.contains("fadeIn")) 
+					i.classList.remove("fadeIn");	
+
+				i.classList.add("display-none");
+			}
+			else{
+				i.classList.add("fadeIn");
+				i.classList.remove("display-none");
+			}
+		})
+	
+		msnry = new Masonry( grid, {});
 }
+
