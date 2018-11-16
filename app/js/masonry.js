5a8ea9338b3458ae3e8cdd680577eaaa252e6a40
VitorTakara <vtakaraa@gmail.com>
2018-11-16 10:03:25 -0200

    Index finalizado

--- app/js/masonry.js
+++ app/js/masonry.js
@@ -1,38 +1,38 @@
-// vanilla JS
-// init with element
+let filtrarMasonry;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    var grid = document.querySelector('.grid');
-	var msnry = new Masonry( grid, {});
+
+   var grid = document.querySelector('.grid');
+   var msnry = new Masonry( grid, {});
+
+   function filtrarMasonry_(portfolioType, btn) {
+      document.querySelectorAll(".btn-portfolio").forEach(i => i.classList.remove("is-active")); // Limpa todos os botões ativos
+      btn.classList.add("is-active"); // Ativa o botão selecionado
+
+      let masonry = document.querySelector(".masonry").querySelectorAll(".grid-item");
+
+      if ( portfolioType == "todos" )
+         masonry.forEach(i => {
+            i.classList.add("fadeIn");
+            i.classList.remove("display-none");
+         })
+      else
+         masonry.forEach(i => {
+            if(!i.classList.contains(portfolioType)){
+               if(i.classList.contains("fadeIn")) 
+                  i.classList.remove("fadeIn");	
+
+               i.classList.add("display-none");
+            }
+            else{
+               i.classList.add("fadeIn");
+               i.classList.remove("display-none");
+            }
+         })
+      
+         msnry = new Masonry( grid, {});
+   }
+
+   filtrarMasonry = filtrarMasonry_;
+
 });
-
-
-
-function filtrarMasonry(portfolioType, btn) {
-	document.querySelectorAll(".btn-portfolio").forEach(i => i.classList.remove("is-active")); // Limpa todos os botões ativos
-	btn.classList.add("is-active"); // Ativa o botão selecionado
-
-	let masonry = document.querySelector(".masonry").querySelectorAll(".grid-item");
-
-	if ( portfolioType == "todos" )
-		masonry.forEach(i => {
-			i.classList.add("fadeIn");
-			i.classList.remove("display-none");
-		})
-	else
-		masonry.forEach(i => {
-			if(!i.classList.contains(portfolioType)){
-				if(i.classList.contains("fadeIn")) 
-					i.classList.remove("fadeIn");	
-
-				i.classList.add("display-none");
-			}
-			else{
-				i.classList.add("fadeIn");
-				i.classList.remove("display-none");
-			}
-		})
-	
-		msnry = new Masonry( grid, {});
-}
-
